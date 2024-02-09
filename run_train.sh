5b9007be1388d5bcdc3b737bc173ace29adf03e6
HYUNMIN-HWANG <hwangkei0212@gmail.com>
2024-02-09 20:48:11 +0900

    add sparc

--- run_train.sh
+++ run_train.sh
@@ -4,4 +4,14 @@ python train.py \
     --save_dir outputs/test \
     --batch_size 100 \
     --log_every -1 \
-    --notes "test"
+    --notes "test"A
+
+
+CUDA_VISIBLE_DEVICES=1 \
+python train.py \
+    --mimic_root /data/wonjun/mimic-cxr-jpg-resized512/ \
+    --save_dir /data/jaayeon/ \
+    --batch_size 128 \
+    --log_every -1 \
+    --notes "test"A \
+    --model sparc
